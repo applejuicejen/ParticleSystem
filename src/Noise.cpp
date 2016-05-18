1acea87db22a8b2a850a8f14a93c51291a4e90e8
jmoorehead <i7611989@bournemouth.ac.uk>
2016-05-18 18:57:54 +0100

    more change

--- src/Noise.cpp
+++ src/Noise.cpp
@@ -8,10 +8,10 @@ namespace Noise
   ngl::Vec3 ComputeNoise(ngl::Vec3 _particle_pos, std::vector <ngl::Vec3> _gradients)
   {
 
-    for(int i = 0; i < _gradients.size(); i ++)
-    {
-      std::cout<<"neighbour gradients: "<<_gradients[i].m_x << ", " << _gradients[i].m_y << ", " << _gradients[i].m_z << std::endl;
-    }
+//    for(int i = 0; i < _gradients.size(); i ++)
+//    {
+//      std::cout<<"neighbour gradients: "<<_gradients[i].m_x << ", " << _gradients[i].m_y << ", " << _gradients[i].m_z << std::endl;
+//    }
     return ngl::Vec3(0,0,0);
   }
 
